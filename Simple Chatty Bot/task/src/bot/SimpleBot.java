8064142375de6dc17d6dc02f2693f09197418fcc
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-19 13:13:09 -0400

    print the line that Hello my name is aid

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! My name is Aid");
+        System.out.println("I was created in 2018");
+        System.out.println("Please, remind me your name.");
         String s = scanner.next();
+        System.out.println("");
     }
 }
