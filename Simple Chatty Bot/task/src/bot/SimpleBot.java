0458d98db62dbacdeac2fee5e26fe53396abe381
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-19 13:07:04 -0400

    print the given lines

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Krishna");
+        System.out.println("I was created in 2002");
+
+
+    }
+}
