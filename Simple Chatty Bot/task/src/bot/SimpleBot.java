34fcfa5078639658e40576186a4326fadab9c72a
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-19 13:24:03 -0400

    calculate age according to given remainders

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -5,10 +5,17 @@ import java.util.Scanner;
 public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! My name is Aid");
-        System.out.println("I was created in 2018");
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
         String s = scanner.next();
-        System.out.println("");
+        System.out.println("What a great name you have, "+s+"!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+        int age = (n1*70 + n2*21 + n3*15)%105;
+        System.out.println("Your age is"+age+"; that's a good time to start programming!");
     }
 }
