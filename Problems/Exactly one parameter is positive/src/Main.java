3ce4819d86f8005e0051828443b6ce8af3652448
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-20 14:23:22 -0400

    check among three numbers exactly one number is positive

--- Problems/Exactly one parameter is positive/src/Main.java
+++ Problems/Exactly one parameter is positive/src/Main.java
@@ -6,6 +6,12 @@ class Main {
         int n1 = scanner.nextInt();
         int n2 = scanner.nextInt();
         int n3 = scanner.nextInt();
+        if ((n1>0) || (n2>0) ||(n3>0)){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
 
         // put your code here
     }
