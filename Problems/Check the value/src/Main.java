a9a49fd7a0e94a8ea4fdbcb508d8e4fec252dcc7
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-20 14:08:36 -0400

    check the value of n if it is greater than 0 and less 10

--- Problems/Check the value/src/Main.java
+++ Problems/Check the value/src/Main.java
@@ -4,6 +4,12 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        if (n<10 && n>0){
+            System.out.println("true");
+        }
+        else {
+            System.out.println("false");
+        }
         // put your code here
     }
 }
