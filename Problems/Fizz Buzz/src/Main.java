b7dbc54cd8924fffcd418b532080113cf4ca2d5d
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-21 15:57:27 -0400

    check if number is divisible by 3 then print Fizz and if divisible by 5 then print Buzz and if divisible by 3 and 5 both then print FizzBuzz

--- Problems/Fizz Buzz/src/Main.java
+++ Problems/Fizz Buzz/src/Main.java
@@ -5,6 +5,18 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
+        System.out.println(n1);
+        for (int i = n1 + 1; i <= n2; i++) {
+            if ((i % 3) == 0) {
+                System.out.println("Fizz");
+            } else if ((i % 5) == 0) {
+                System.out.println("Buzz");
+            } else if ((i % 3) == 0 && (i % 5) == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(i);
+            }
+        }
         // put your code here
     }
 }
