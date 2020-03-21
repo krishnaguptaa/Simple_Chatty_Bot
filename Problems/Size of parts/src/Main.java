7f3eb3c4904023e8212c1e7dbbc396a9a0609b95
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-21 17:00:25 -0400

    input no. of parts

---
+++ Problems/Size of parts/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+
+
+        // put your code here
+    }
+}
