fc190227132f9c038d8f89b15f67b897102f092a
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-21 13:41:29 -0400

    check whether the number is positive or negative

--- Problems/Check a number is positive/src/Main.java
+++ Problems/Check a number is positive/src/Main.java
@@ -4,6 +4,11 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        if (n > 0) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
         // put your code here
     }
 }
