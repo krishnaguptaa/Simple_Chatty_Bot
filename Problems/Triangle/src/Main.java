30f7c8a8ea9008b3dbb29cd501cc8b3e59489516
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-21 13:52:56 -0400

    take 3 integer variables and store in it the sum of n1&n2 and so on

--- Problems/Triangle/src/Main.java
+++ Problems/Triangle/src/Main.java
@@ -6,6 +6,10 @@ class Main {
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         int n3 = sc.nextInt();
+        int a = n1 + n2;
+        int b = n2 + n3;
+        int c = n3 + n1;
+
 
         // put your code here
     }
