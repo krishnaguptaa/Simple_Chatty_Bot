9de93d3fe6314eaea16d30457b08abb9193f82a0
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-22 18:45:52 -0400

    put a while loop and in it check if number is equal to zero then break and if number is completely divisible by 2 then print even else odd.

---
+++ Problems/Even or odd/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) {
+                break;
+            }
+            if (n % 2 == 0) {
+                System.out.println("even");
+            }
+            if ((n % 2) != 0) {
+                System.out.println("odd");
+            }
+
+        }
+        // put your code here
+    }
+}
