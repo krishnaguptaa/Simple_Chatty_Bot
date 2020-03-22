229edf73dbaf7f898c4220bded994b0871f6ddae
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-22 16:44:36 -0400

    declare a array of n numbers

--- Problems/The sequence/src/Main.java
+++ Problems/The sequence/src/Main.java
@@ -4,6 +4,11 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int[] arr = new int[n];
+            arr[i] = sc.nextInt();
+        }
+
         // put your code here
     }
 }
