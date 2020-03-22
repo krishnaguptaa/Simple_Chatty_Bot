1c32c9fe02b1644d0f52c46fbe326bec64b0d946
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-22 17:47:43 -0400

    print squares of upcoming natural numbers.

--- Problems/Squares of natural numbers/src/Main.java
+++ Problems/Squares of natural numbers/src/Main.java
@@ -4,6 +4,10 @@ class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        for (int i = 1; i < n; i++) {
+            int n1 = i * i;
+            System.out.println(i);
+        }
         // put your code here
     }
 }
