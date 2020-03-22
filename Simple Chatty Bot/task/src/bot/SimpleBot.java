ef6d48efe9e40794777b9a1e5af21b3851487f0e
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-22 15:07:47 -0400

    take input a number

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -24,6 +24,7 @@ public class SimpleBot {
 
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
         System.out.println("Now I will prove to you that I can count to any number you want.");
+        int n = scanner.nextInt();
 
         // read a number and count to it here
 
