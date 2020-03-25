2cd80d64ee0bbe6e303484fca4cd19f3713d8be8
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-25 16:50:38 -0400

    check if bus height is bigger than bridge height then value of bc will be true

--- Problems/Bus tour/src/Main.java
+++ Problems/Bus tour/src/Main.java
@@ -5,7 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int bh = sc.nextInt();
         int b = sc.nextInt();
-
+        Boolean bc = false;
+        int bca = 0;
+        for (int i = 1; i <= b && !bc; i++) {
+            int brh = sc.nextInt();
+            if (bh >= brh) {
+                bc = true;
+                bca = i;
+            }
+        }
 
         // write your code here
     }
