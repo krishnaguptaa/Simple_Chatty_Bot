b9ce4d2ce2209d6fc1dec34a3b287d02604be118
krishnaguptaa <krishna2018gupta@gmail.com>
2020-03-25 16:17:22 -0400

    take input no. of bridge

--- Problems/Bus tour/src/Main.java
+++ Problems/Bus tour/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int bh = sc.nextInt();
+        int b = sc.nextInt();
 
 
         // write your code here
